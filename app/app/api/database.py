03eba6e5b133b91d83b7ae76cca0a723bacb125a
RuiZheYang1991 <91766837+RuiZheYang1991@users.noreply.github.com>
2023-08-28 07:55:21 +0800

    Delete app directory

--- app/app/api/database.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
-#set FLASK_APP=run.py
-# flask --app run.py db init
-# flask --app run.py db migrate -m "Initial migration"
-#flask --app run.py db upgrade
--- app/app/api/views.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, Blueprint
-
-views_bp = Blueprint('views', __name__)
-@views_bp.route('/')
-def index():
-    return render_template('index.html')
